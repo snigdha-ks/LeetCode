cd6e82b13c5f77948f2feb09f72ad61b3a4bc458
snigdha-ks <110383462+snigdha-ks@users.noreply.github.com>
2024-07-18 00:00:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        
+        if ransomNote in magazine:
+            return True
+        else:
+            return False
